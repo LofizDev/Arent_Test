85c3b08996264d04a132b16c82d854c0c098fe3b
Lam Nhat Khoi <khoilam.dev@gmail.com>
2023-07-11 00:11:48 +0700

    filter + loadmore

--- src/components/FoodInfo.tsx
+++ src/components/FoodInfo.tsx
@@ -5,7 +5,7 @@ interface IProps {
 function FoodInfo(props: IProps) {
     const { title, url } = props
     return (
-        <div className='w-full relative h-full'>
+        <div className='w-full relative h-[241px]'>
             <img className='w-full h-full object-cover' src={url} alt="food" />
             <span className='w-[120px] h-[32px] bg-primary-300 absolute bottom-0 left-0 font-inter text-[15px] text-white flex justify-center items-center'>
                 {title}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,20 +2,28 @@ import React, { useState } from 'react'
 import FoodInfo from '../components/FoodInfo'
 import Hexagon from '../components/Hexagon'
 import { categories, meal } from '../../mock/foods'
+import LinearButton from '../components/LinearButton'
 function HomePage() {
+    const [expand, setExpand] = useState<boolean>(false)
     const [active, setActive] = useState<string>('all')
     return (
         <div>
-            <div className='flex items-center w-[700px] justify-between'>
+            <div className='flex mx-auto cursor-pointer items-center w-[700px] justify-between'>
                 {categories.map(e => (
                     <Hexagon onClick={() => setActive(e.name)} key={e.name} type={e.type} name={e.name} />
                 ))}
             </div>
-            <div className='w-[960px] mx-auto gap-[6px] grid grid-cols-4'>
-                {meal.filter(el => active !== 'all' ? el.category == active : true).map(e => (
-                    <FoodInfo title={e.title} url={e.url} />
-                ))}
-
+            <div className='w-[960px] mt-[50px] flex flex-col items-center mx-auto'>
+                <div className='gap-[6px] grid grid-cols-4'>
+                    {meal.filter(el => active !== 'all' ? el.category == active : true).slice(0, expand ? meal.length : 8).map(e => (
+                        <FoodInfo title={e.title} url={e.url} />
+                    ))}
+                </div>
+                {!expand && (
+                    <div onClick={() => setExpand(true)} className='mt-5 mx-auto'>
+                        <LinearButton name="記録をもっと見る" />
+                    </div>
+                )}
             </div>
         </div>
     )
