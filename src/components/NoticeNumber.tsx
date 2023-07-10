22a71e848be340114d6c204b9c47b4093ee118b8
Lâm Nhật Khôi <khoilam.dev@gmail.com>
2023-07-11 02:40:16 +0700

    remove hard code

--- src/components/NoticeNumber.tsx
+++ src/components/NoticeNumber.tsx
@@ -8,7 +8,7 @@ interface Props {
 function NoticeNumber({ children, number }: Props) {
     return (<div className="relative">
         {children}
-        <div className="absolute bg-primary-500 h-[16px] w-[16px] rounded-full flex justify-center items-center text-[10px] top-0 -right-1/4">
+        <div className="absolute bg-primary-500 h-[16px] w-[16px] rounded-full flex justify-center items-center text-[10px] top-0 -right-1/4 text-light">
                 {number}
             </div>
     </div>)
