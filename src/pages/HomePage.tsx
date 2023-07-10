c7f65aa11a418ea685942841fde67b7ad6111d8b
Lam Nhat Khoi <khoilam.dev@gmail.com>
2023-07-11 02:03:15 +0700

    update dynamic chart data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,10 +8,11 @@ import Layout from '../layout'
 function HomePage() {
     const [expand, setExpand] = useState<boolean>(false)
     const [active, setActive] = useState<string>('all')
+    const [data] = useState({ data1: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6], data2: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8] })
     return (
         <Layout>
             <div>
-                <Chart />
+                <Chart data={data} />
                 <div className='flex mx-auto cursor-pointer items-center w-[700px] justify-between mt-10'>
                     {categories.map(e => (
                         <Hexagon onClick={() => setActive(e.name)} key={e.name} type={e.type} name={e.name} />
