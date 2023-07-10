22452bb81f3e58681191d20ff3b6d79626f687a5
Lam Nhat Khoi <khoilam.dev@gmail.com>
2023-07-10 23:09:32 +0700

    Component Hexagon

---
+++ src/pages/TestComponent.tsx
@@ -0,0 +1,14 @@
+import Hexagon from "../components/Hexagon";
+
+function TestComponent() {
+  return (
+    <div>
+      <Hexagon type="knife" name="Morning" />
+      <Hexagon type="knife" name="Lunch" />
+      <Hexagon type="knife" name="Dinner" />
+      <Hexagon type="cup" name="Snack" />
+    </div>
+  );
+}
+
+export default TestComponent;
